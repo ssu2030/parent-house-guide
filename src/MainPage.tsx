b61f77f2301af76d3ebe5af5803bdafecd359343
dongho <ssu2030@naver.com>
2022-07-12 00:34:48 +0900

    add test

---
+++ src/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './style/MainPage.css'
+
+const MainPageComponent: React.FC = () => {
+
+    return <div
+        className="Main"
+    >
+        hello
+    </div>;
+
+}
+
+export default MainPageComponent;
