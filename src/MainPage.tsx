f0a855aeb334d9fa9260262307c8068304bf4282
dongho <ssu2030@naver.com>
2022-07-12 01:20:32 +0900

    fix

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -6,21 +6,11 @@ import Button from '@material-ui/core/Button';
 const MainPageComponent: React.FC = () => {
 
     return (
-    <div
-        className="Main"
-    >
-       <Button id={"homeButton"} variant="outlined" className="HomeButton">
-            {"홈"}
-        </Button>
-        <Button id={"introButton"} variant="outlined" className="HomeButton">
-            소개
-        </Button>
-        <Button id={"warnButton"} variant="outlined" className="HomeButton">
-            주의사항
-        </Button>
-        <Button id={"wayButton"}  variant="outlined" className="HomeButton">
-            오시는 길
-        </Button>
+    <div className="Main">
+        <Button id={"homeButton"} variant="outlined" className="HomeButton">홈</Button>
+        <Button id={"introButton"} variant="outlined" className="HomeButton">소개</Button>
+        <Button id={"warnButton"} variant="outlined" className="HomeButton">주의사항</Button>
+        <Button id={"wayButton"}  variant="outlined" className="HomeButton">오시는 길</Button>
     </div>)
     ;
 
