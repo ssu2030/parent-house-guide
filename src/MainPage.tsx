2faa637cf257da685588b8aaca0cee4c8171c09a
dongho <ssu2030@naver.com>
2022-07-12 01:08:54 +0900

    fix style

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -8,8 +8,17 @@ const MainPageComponent: React.FC = () => {
     return <div
         className="Main"
     >
-        <Button>
-
+       <Button id={"homeButton"} variant="outlined" className="HomeButton">
+            {"홈"}
+        </Button>
+        <Button id={"introButton"} variant="outlined" className="HomeButton">
+            소개
+        </Button>
+        <Button id={"warnButton"} variant="outlined" className="HomeButton">
+            주의사항
+        </Button>
+        <Button id={"wayButton"}  variant="outlined" className="HomeButton">
+            오시는 길
         </Button>
     </div>;
 
