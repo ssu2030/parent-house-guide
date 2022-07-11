168d03f752e983d80272923ccc5599800b8c58bb
dongho <ssu2030@naver.com>
2022-07-12 00:52:59 +0900

    add Button

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 import './style/MainPage.css'
+import Button from '@material-ui/core/Button';
 
 const MainPageComponent: React.FC = () => {
 
     return <div
         className="Main"
     >
-        hello
+        <Button>
+
+        </Button>
     </div>;
 
 }
