946f69af13b853da11015d70c53134161d926226
dongho <ssu2030@naver.com>
2022-07-12 01:34:25 +0900

    add button

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -4,13 +4,22 @@ import './style/MainPage.css'
 import Button from '@material-ui/core/Button';
 
 const MainPageComponent: React.FC = () => {
+    
 
     return (
     <div className="Main">
-        <Button id={"homeButton"} variant="outlined" className="HomeButton">홈</Button>
-        <Button id={"introButton"} variant="outlined" className="HomeButton">소개</Button>
-        <Button id={"warnButton"} variant="outlined" className="HomeButton">주의사항</Button>
-        <Button id={"wayButton"}  variant="outlined" className="HomeButton">오시는 길</Button>
+        <div className="ButtonWrapper">
+            <Button id={"homeButton"} variant="contained" className="HomeButton">홈</Button>
+        </div>
+        <div className="ButtonWrapper">
+            <Button id={"introButton"} variant="contained" className="HomeButton">소개</Button>
+        </div>
+        <div className="ButtonWrapper">
+        <Button id={"warnButton"} variant="contained" className="HomeButton">주의사항</Button>
+        </div>
+        <div className="ButtonWrapper">
+        <Button id={"wayButton"}  variant="contained" className="HomeButton">오시는 길</Button>
+        </div>
     </div>)
     ;
 
