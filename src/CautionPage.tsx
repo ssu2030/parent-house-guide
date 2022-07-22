9c064fe6750cd04317798e21635673cf2b141568
dongho_lee <dongho_lee@tmax.co.kr>
2022-07-22 17:08:30 +0900

    resize img files size

---
+++ src/CautionPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './style/MapPage.css'
+
+
+const CautionPageComponent: React.FC = () => {
+    
+    return (
+        <div className="MapOutDivision">
+            주의 사항
+        </div>
+        );
+
+}
+
+export default CautionPageComponent;
+
+
+
