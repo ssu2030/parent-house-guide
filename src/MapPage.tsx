72647c35558fc49e1d4338c351874b40ca213e2f
dongho_lee <dongho_lee@tmax.co.kr>
2022-07-22 15:45:32 +0900

    add style

---
+++ src/MapPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import './style/MapPage.css'
+
+
+const MapPageComponent: React.FC = () => {
+    
+
+    
+    return (
+    <div className="OutDivision">
+        <div id='myMap' style={{
+            width: '420px',
+            height: '420px',
+        }}></div>
+       
+    </div>);
+
+}
+
+export default MapPageComponent;
+
+
+
