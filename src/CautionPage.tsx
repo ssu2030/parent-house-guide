d8b244c68d8dcd1f9049413f87f12e7e9a01268b
dongho <ssu2030@naver.com>
2022-07-24 19:49:46 +0900

    add space on string

--- src/CautionPage.tsx
+++ src/CautionPage.tsx
@@ -18,24 +18,24 @@ const CautionPageComponent: React.FC = () => {
             <div className="StringDivision">
                 <img alt={"inside"} style={{height: "30px"}} src={inside}/>
                 <br/>
-                    ‣ 현관문 사이의 중문을 꼭 닫고 다녀야 합니다. 
+                &nbsp;&nbsp;‣ 현관문 사이의 중문을 꼭 닫고 다녀야 합니다. 
                 <br/>
-                    ‣ 휴지는 변기에 버리는 것이 아니라, 휴지통에 버려주셔야 합니다.
+                &nbsp;&nbsp;‣ 휴지는 변기에 버리는 것이 아니라, 휴지통에 버려주셔야 합니다.
                 <br/>
             </div>
             <div className="StringDivision">
-                <img alt={"outside"} style={{width: "30px"}} src={outside}/>
+                <img alt={"outside"} style={{height: "30px"}} src={outside}/>
                 <br/>
-                    ‣ 꽃밭에 꽃을 꺾거나, 밟지 않도록 조심하셔야 합니다. 
+                &nbsp;&nbsp;‣ 꽃밭에 꽃을 꺾거나, 밟지 않도록 조심하셔야 합니다. 
                 <br/>
-                    ‣ 꽃밭이나 외부에 쓰레기를 버리시면 안됩니다. 
+                &nbsp;&nbsp;‣ 꽃밭이나 외부에 쓰레기를 버리시면 안됩니다. 
                 <br/>
-                    ‣ 밤에 외출시엔 라이트를 켜 뱀이 있는지 없는지 확인해야 합니다.
+                &nbsp;&nbsp;‣ 밤에 외출시엔 라이트를 켜 뱀이 있는지 없는지 확인해야 합니다.
                 <br/>
-                    ‣ 돌담을 차거나 무너뜨리는 행위를 하시면 안됩니다.
+                &nbsp;&nbsp;‣ 돌담을 차거나 무너뜨리는 행위를 하시면 안됩니다.
                 <br/>
             </div>
-            
+
         </div>
         );
 
--- src/DescriptionPage.tsx
+++ src/DescriptionPage.tsx
@@ -47,12 +47,7 @@ const DescriptionPageComponent: React.FC = () => {
             </SwiperSlide>
         </Swiper>
         <div className="StringDivision">
-            maison de essense
-        </div>
-        <div className="StringDivision">
-            부모님이 퇴직 후 안락한 노후 생활을 보내기 위해 지어진 집입니다.
-            <br/>
-            2020년 부터, 짓기 시작한 집은 약 1년에 걸쳐 완공되었습니다. 
+            &nbsp;부모님이 퇴직 후 안락한 노후 생활을 보내기 위해 지어진 집입니다. 2020년 부터, 짓기 시작한 집은 약 1년에 걸쳐 완공되었습니다. 
             <br/>
             꽃을 좋아하는 어머니와, 주변의 자연을 좋아하는 아버지에게 어울리는 느낌으로 지은 집입니다.
             <br/>
@@ -72,7 +67,7 @@ const DescriptionPageComponent: React.FC = () => {
             </SwiperSlide>
         </Swiper>
         <div className="StringDivision">
-            봄, 여름, 가을, 겨울 부모님께서 가꾼 꽃들을 계절별로 구경할 수 있습니다. 뿐만아니라 집 뒤편 하우스에선 부모님께서 좋아하는 과일들을 심어 가꾸고 계십니다.
+        &nbsp;봄, 여름, 가을, 겨울 부모님께서 가꾼 꽃들을 계절별로 구경할 수 있습니다. 뿐만아니라 집 뒤편 하우스에선 부모님께서 좋아하는 과일들을 심어 가꾸고 계십니다.
             <br/>
             어머니께서 특히 좋아하시는 화단은 부모님께서 함께 관리하십니다. 뿐만아니라 집주변 작은 돌담들은 아버지와 어머니께서 직접 쌓으셨습니다.
         </div>
@@ -85,7 +80,7 @@ const DescriptionPageComponent: React.FC = () => {
             </SwiperSlide>
         </Swiper>
         <div className="StringDivision">
-            하우스의 여러 채소와 밭에 호박, 깨 등의 작물등을 키우며 관리하고 있습니다.
+        &nbsp; 하우스의 여러 채소와 밭에 호박, 깨 등의 작물등을 키우며 관리하고 있습니다.
             <br/>
         </div>
         <Swiper className="Swiper" >
@@ -97,7 +92,7 @@ const DescriptionPageComponent: React.FC = () => {
             </SwiperSlide>
         </Swiper>
         <div className="StringDivision">
-            집 앞으로 조금 내려가면 작은 계곡이 있습니다. 
+        &nbsp;집 앞으로 조금 내려가면 작은 계곡이 있습니다. 
             <br/>
             물놀이를 즐길 수 있고 낚시를 즐길 수 있습니다. 송사리등 민물고기등을 잡을 수 있습니다.
         </div>
