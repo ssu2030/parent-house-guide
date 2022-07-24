fad97b16ac610f3d849e5729f9d8a8c23e154add
dongho <ssu2030@naver.com>
2022-07-24 20:25:36 +0900

    bird test

--- src/CautionPage.tsx
+++ src/CautionPage.tsx
@@ -35,7 +35,20 @@ const CautionPageComponent: React.FC = () => {
                 &nbsp;&nbsp;‣ 돌담을 차거나 무너뜨리는 행위를 하시면 안됩니다.
                 <br/>
             </div>
-
+            <div className="container">	
+	        <div className="bird-container bird-container--one">
+            	<div className="bird bird--one"></div>
+    	    </div>
+	        <div className="bird-container bird-container--two">
+		        <div className="bird bird--two"></div>
+	        </div>
+	        <div className="bird-container bird-container--three">
+		        <div className="bird bird--three"></div>
+	        </div>
+	        <div className="bird-container bird-container--four">
+		        <div className="bird bird--four"></div>
+	        </div>
+        </div>
         </div>
         );
 
